e13d37de3216b303ccfcd7c2856d828786356131
Justina18 <igbanijustina@gmail.com>
2023-02-01 07:47:32 +0100

    changes made

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 import {MdLocationPin} from 'react-icons/md'
 import './Book.css'
 import {BsSuitHeartFill,BsSuitHeart} from 'react-icons/bs'
 
 const Book = () => {
+
+  const navigate = useNavigate()
+  const [toggle, setToggle] = useState(true);
+
+  const handleToggle = () => { setToggle(!toggle) }
+  const FiAlignJustif = (<BsSuitHeart  onClick={handleToggle} fontSize={15}/> )
+  const FaTime = (<div>
+      <BsSuitHeartFill color='E22727'onClick={handleToggle}  fontSize={15}/> 
+                </div>)
+
   return (
     <div  className='book'>
       <div className='book-wrap'>
-        <h1>Available Doctors</h1>
+        <div className='book-head'>
+          <img onClick={()=> navigate('/')} className='book-head-img' src='/head-log.jpeg' />
+          <h1>Available Doctors</h1> 
+          <h4>Favorites <BsSuitHeartFill color='E22727' fontSize={15}/> </h4>
+        </div>
+        <hr className='book-line'/>
         <div className='wrapped'>
         <div  className='book-wrap-main'>
           <div className='book-img-wrap'>
@@ -16,9 +32,9 @@ const Book = () => {
           <div>
           <div className='book-text'>
             <h2 className='book-h' >
-              Joseph Jonah <BsSuitHeartFill color='E22727' fontSize={15}/> 
+              Joseph Jonah {toggle ? FiAlignJustif : FaTime}
             </h2>
-            <p>Dietitian</p>
+            <p>Anatomist</p>
             <MdLocationPin color='#1B75BC'/>Abuja
             <hr/>
           </div>
@@ -38,53 +54,7 @@ const Book = () => {
           <div>
           <div className='book-text'>
             <h2 className='book-h'>
-              Joseph Jonah <BsSuitHeart fontSize={15}/>
-            </h2>
-            <p>Dietitian</p>
-            <MdLocationPin color='#1B75BC'/>Abuja
-            <hr/>
-          </div>
-            <div className='book-foot'>
-             <p> $1500</p>
-             <button className='book-butt'>
-                Book Now
-             </button>
-            </div>
-          </div>
-        </div>
-        </div>
-
-        <div className='wrapped'>
-        <div  className='book-wrap-main'>
-          <div className='book-img-wrap'>
-            <img  className='book-img' src='/book.jpg'/>
-          </div>
-          <div>
-          <div className='book-text'>
-            <h2 className='book-h'>
-              Joseph Jonah<BsSuitHeart fontSize={15}/>
-            </h2>
-            <p>Dietitian</p>
-            <MdLocationPin color='#1B75BC'/>Abuja
-            <hr/>
-          </div>
-            <div className='book-foot'>
-             <p> $1500</p>
-             <button className='book-butt'>
-                Book Now
-             </button>
-            </div>
-          </div>
-        </div>
-
-        <div  className='book-wrap-main'>
-          <div className='book-img-wrap'>
-            <img  className='book-img' src='/book.jpg'/>
-          </div>
-          <div>
-          <div className='book-text'>
-            <h2 className='book-h'>
-              Joseph Jonah <BsSuitHeartFill color='E22727' fontSize={15}/> 
+              Joseph Jonah  {toggle ? FiAlignJustif : FaTime}
             </h2>
             <p>Dietitian</p>
             <MdLocationPin color='#1B75BC'/>Abuja
@@ -108,7 +78,7 @@ const Book = () => {
           <div>
           <div className='book-text'>
             <h2 className='book-h'>
-              Joseph Jonah <BsSuitHeart fontSize={15}/>
+              Joseph Jonah {toggle ? FiAlignJustif : FaTime}
             </h2>
             <p>Dietitian</p>
             <MdLocationPin color='#1B75BC'/>Abuja
@@ -130,7 +100,53 @@ const Book = () => {
           <div>
           <div className='book-text'>
             <h2 className='book-h'>
-              Joseph Jonah <BsSuitHeartFill color='E22727' fontSize={15}/> 
+              Joseph Jonah  {toggle ? FiAlignJustif : FaTime}
+            </h2>
+            <p>Dietitian</p>
+            <MdLocationPin color='#1B75BC'/>Abuja
+            <hr/>
+          </div>
+            <div className='book-foot'>
+             <p> $1500</p>
+             <button className='book-butt'>
+                Book Now
+             </button>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        <div className='wrapped'>
+        <div  className='book-wrap-main'>
+          <div className='book-img-wrap'>
+            <img  className='book-img' src='/book.jpg'/>
+          </div>
+          <div>
+          <div className='book-text'>
+            <h2 className='book-h'>
+              Joseph Jonah  {toggle ? FiAlignJustif : FaTime}
+            </h2>
+            <p>Dietitian</p>
+            <MdLocationPin color='#1B75BC'/>Abuja
+            <hr/>
+          </div>
+            <div className='book-foot'>
+             <p> $1500</p>
+             <button className='book-butt'>
+                Book Now
+             </button>
+            </div>
+          </div>
+        </div>
+
+        <div  className='book-wrap-main'>
+          <div className='book-img-wrap'>
+            <img  className='book-img' src='/book.jpg'/>
+          </div>
+          <div>
+          <div className='book-text'>
+            <h2 className='book-h'>
+              Joseph Jonah  {toggle ? FiAlignJustif : FaTime}
             </h2>
             <p>Dietitian</p>
             <MdLocationPin color='#1B75BC'/>Abuja
--- src/Components/Prem/Prem.jsx
+++ src/Components/Prem/Prem.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import {CgShapeZigzag} from 'react-icons/cg'
+import {RiVipCrown2Line} from 'react-icons/ri'
+import './Prem.css'
 
 const Prem = () => {
   return (
-    <div>Prem</div>
+    <div className='prem'>
+      <div className='prem-wrap'>
+      <div className='prem-hold'>
+          <h1 className='prem-h1' >
+              Premium <RiVipCrown2Line  color='#FDD13F' fontSize={25}/>
+             </h1><CgShapeZigzag fontSize={25}/>
+             <p className='prem-p' >
+            The Health360 platform <br/> makes it possible for anyone with <br/> a smartphone or tablet to receive <br/> quality and affordable health <br/> consultation.
+             </p>
+          </div>
+
+          <div className='prem-hold'>
+          <h1 className='prem-h1' >
+          Premium <RiVipCrown2Line  color='#FDD13F' fontSize={25}/>
+             </h1><CgShapeZigzag fontSize={25}/>
+             <p className='prem-p' >
+            The Health360 platform <br/> makes it possible for anyone with <br/> a smartphone or tablet to receive <br/> quality and affordable health <br/> consultation.
+             </p>
+          </div>
+          
+          
+          <div className='prem-hold'>
+          <h1 className='prem-h1' >
+          Premium <RiVipCrown2Line  color='#FDD13F' fontSize={25}/>
+             </h1><CgShapeZigzag fontSize={25}/>
+             <p className='prem-p' >
+            The Health360 platform <br/> makes it possible for anyone with <br/> a smartphone or tablet to receive <br/> quality and affordable health <br/> consultation.
+             </p>
+          </div>
+      </div>
+      
+    </div>
   )
 }
 
